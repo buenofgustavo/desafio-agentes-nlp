a4df8597ccc0ee468c10e609e71474126dfec263
Igor Reis Braziel <ireis@tce.go.gov.br>
2026-04-15 14:23:44 -0300

    update: document ingestion

--- src/indexing/document_pipeline.py
+++ src/indexing/document_pipeline.py
@@ -1,11 +1,30 @@
 """Responsável por orquestrar o processo de ingestão dos documentos, desde a extração do texto até a geração dos chunks e embeddings."""
+import time
 from src.indexing.ingestion.json_loader import JsonLoader
-from src.indexing.ingestion.pdf_loader import PdfDownloader
+from src.indexing.ingestion.document_downloader import DocumentDownloader
 from src.core.config import Constants
+from src.utils.logger import LoggingService
 
-def main() -> None:
-    all_aneel_records = JsonLoader.load_json_folder_data(Constants.JSON_DIR)
-    PdfDownloader.download_pdfs(all_aneel_records)
+logger = LoggingService.setup_logger(__name__)
+
+class DocumentPipeline:
+    """Pipeline de ingestão de documentos legislativos."""
+    
+    @staticmethod
+    def execute() -> None:
+        """Executa o pipeline completo de ingestão."""
+        logger.info("Iniciando pipeline de ingestão.")
+
+        start_time = time.time()
+        all_aneel_records = JsonLoader.load_json_folder_data(Constants.JSON_DIR)
+        duration = time.time() - start_time
+        logger.info(f"Extração de dados JSON concluída em {duration:.2f} segundos. Total de registros: {len(all_aneel_records)}")
+        
+        start_time = time.time()
+        with DocumentDownloader() as document_downloader:
+            document_downloader.download_documents(records=all_aneel_records)
+        duration = time.time() - start_time
+        logger.info(f"Download de documentos concluído em {duration/60:.2f} minutos.")
 
 if __name__ == "__main__":
-    main()
+    DocumentPipeline.execute()
